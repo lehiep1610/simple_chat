760e3acf0e0c1d8abb5ba9a4e110dd5485ead07d
HiepLe <hieple.1606@gmail.com>
2026-02-25 11:55:57 +0700

    add socket for chat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,24 @@
+
+import { createServer } from 'http';
 import app from './app';
 import { initDatabase } from './core/config/database';
+import { Server } from 'socket.io';
+import { setupChatSocket } from './features/chat/presentation/routes/chat.routes';
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
     await initDatabase();
+
+    const httpServer = createServer(app);
+    const io = new Server(httpServer, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST'],
+        },
+    });
+
+    setupChatSocket(io);
 
     app.listen(PORT, () => {
         console.log(`🚀 Server running at http://localhost:${PORT}`);
